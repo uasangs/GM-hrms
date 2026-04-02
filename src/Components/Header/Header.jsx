340ad529ad675b108216190294f9dbcf5180df86
AbhiishekSingh <imabhishekk20@gmail.com>
2026-04-02 16:09:31 +0530

    logo Changed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import Notification  from  "../../assets/Notification.png";
-// import Queries  from  "../../assets/Queries.png";
-// import ChatForm from './ChatForm';
-// function Header() {
-//   return (
-//     <div>
-//        <div className="dashboard-header">
-//              <h4>Employe Self Service</h4> 
-//              <div className="top-right-icons">
-//              <div> <p>FD00000</p></div> 
-//                <div> <img src={Notification} alt="" /></div>  
-//                  {/* <div> <ChatForm/> </div> */}
-//              </div>
-            
-//           </div>
-//     </div>
-//   )
-// }
-
-// export default Header
-
-
-
 import React, { useState, useEffect } from "react";
 import Notification from "../../assets/Notification.png";
 import Queries from "../../assets/Queries.png";
